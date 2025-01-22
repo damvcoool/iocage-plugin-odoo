110ef66ecaf5e956520298ef1fa54b96ecadbb30
Daniel Mejia <2473632+damvcoool@users.noreply.github.com>
2025-01-22 07:22:49 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
+sysrc postgresql_enable=YES 2>/dev/null
+
+sysrc odoo_enable=YES 2>/dev/null
+
+sysrc odoo_database="odoodb" 2>/dev/null
+sysrc odoo_datadir="/var/db/odoo" 2>/dev/null
+
+chmod 777 /tmp
+# Start the service
+service postgresql initdb 2>/dev/null
+sleep 5
+service postgresql start 2>/dev/null
+sleep 5
+
+USER="odoouser"
+DB="odoodb"
+
+# Save the config values
+echo "$DB" > /root/dbname
+echo "$USER" > /root/dbuser
+export LC_ALL=C
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
+PASS=`cat /root/dbpassword`
+
 echo "Done"
