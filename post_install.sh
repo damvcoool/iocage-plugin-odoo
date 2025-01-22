c5e5478e49cbdfa073e7e8e8314fa9ef2a304301
Daniel Mejia <2473632+damvcoool@users.noreply.github.com>
2025-01-22 16:34:52 -0500

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -24,4 +24,20 @@ export LC_ALL=C
 cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1 > /root/dbpassword
 PASS=`cat /root/dbpassword`
 
+# create db user
+psql -d template1 -U postgres -c "CREATE USER ${USER} CREATEDB SUPERUSER;" 2>/dev/null
+# Set a password on the postgres account
+psql -d template1 -U postgres -c "ALTER USER ${USER} IDENTIFIED WITH caching_sha2_password BY '${PASS}';" 2>/dev/null
+
+service odoo initdb
+
+service odoo start
+
+# Save database information
+echo "Host: localhost or 127.0.0.1" > /root/PLUGIN_INFO
+echo "Database Type: PostgresSQL" >> /root/PLUGIN_INFO
+echo "Database Name: $DB" >> /root/PLUGIN_INFO
+echo "Database User: $USER" >> /root/PLUGIN_INFO
+echo "Database Password: $PASS" >> /root/PLUGIN_INFO
+
 echo "Done"
